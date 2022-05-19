3f1fd8e4dd2d65283665e37d569015cdb2a7dc04
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-18 20:52:16 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -55,11 +55,9 @@ function capitalizeWords(arr) {
   // base case
   if (arr.length === 1) {
     return [arr[0].toUpperCase()];
-
   }
   // main logic
-  let newArr = capitalizeWords(arr.slice(0, -1))
-  newArr.push(arr.slice(arr.length - 1)[0].toUpperCase()));
+  let newArr = capitalizeWords(arr.slice(0, -1));
+  newArr.push(arr.slice(arr.length - 1)[0].toUpperCase());
   return newArr;
-  }
 }
